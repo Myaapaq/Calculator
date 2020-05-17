ba4fed22481c7090759d50979d12f83fc728f8a9
Mya <51803468+Myaapaq@users.noreply.github.com>
2020-05-17 16:07:40 -0500

    Add operator function

--- calculator.js
+++ calculator.js
@@ -1,39 +1,41 @@
 function add (x, y) {
-	return x + y;
+	console.log(x + y);
 }
 function subtract (x, y) {	 
-	return x - y ; 
+	console.log(x - y );
+	//return x - y ; 
 }
-function multiply (array) {
-	return array.reduce((current, next) => current * next);
-}
-function divide( x, y) {
-    return x / y 
-}
-//add the current total to any other yer
-function sum (array) {
-    return array.reduce((current, brandNew)=> current + brandNew, 0 );
-} 
-//test doc stuff
-function add (x, y) {
-	return x + y;
-}
-function subtract (x, y) {	 
-	return x - y ; 
-}
-function multiply (array) {
-	return array.reduce((current, next) => current * next);
+function multiply (x, y) {
+	//return array.reduce((current, next) => current * next);
+	console.log(x * y );
 }
 function divide( x, y) {
-	return x / y 
+	//return x / y 
+	console.log(x / y );
 }
-//add the current total to any other yer
+//add the current total to any other number
 function sum (array) {
 	return array.reduce((current, brandNew)=> current + brandNew, 0 );
 } 
 function operator(operator, x, y){
-	
+	if(operator == '+'){
+		add(x, y)
+	} else if (operator == '-'){
+		subtract(x, y)
+	} else if (operator == '*'){
+		multiply(x, y)
+	} else if (operator == '/'){
+		divide (x, y)
+	}else{ console.log('try again')}
 }
+
+/*
+alternate multiply function
+function multiply (array) {
+	return array.reduce((current, next) => current * next);
+}
+*/
+
 
 
 
